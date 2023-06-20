2c5b4e4b871a91d85eec3cba95ca381c8b691f6d
Wardi Soulaimana <wardi.soulaimana@igt.com>
2023-06-20 16:41:02 +0100

    sidebar

---
+++ components/Box.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from "tailwind-merge";
+
+const Box = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={twMerge(
+        `bg-neutral-900
+          rounded-lg
+          h-fit
+          w-full
+        `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;

2b7bc0b4a0599b10bad7bb5273ef2667f00d1913
Wardi Soulaimana <wardi.soulaimana@igt.com>
2023-07-02 16:54:39 +0100

    player hooks

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 
 import React from "react";
@@ -10,6 +11,8 @@ interface PageContentProps {
 }
 
 const PageContent = ({ songs }: PageContentProps) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available.</div>;
   }
@@ -17,7 +20,11 @@ const PageContent = ({ songs }: PageContentProps) => {
   return (
     <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8">
       {songs.map((item) => (
-        <SongItem key={item.id} onClick={() => {}} data={item} />
+        <SongItem
+          key={item.id}
+          onClick={(id: string) => onPlay(id)}
+          data={item}
+        />
       ))}
     </div>
   );
